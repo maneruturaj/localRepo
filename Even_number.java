8a6e6734326c1430088c00bdb9ce865e2d4768bf
Maneruturaj <mane.ruturaj4@gmail.com>
2024-10-26 21:36:19 +0530

    Even Number

---
+++ Even_number.java
@@ -0,0 +1,16 @@
+package RBI_Bank;
+
+public class Even_number
+{
+	public static void main(String[]args)
+	{
+		int s=11;
+		for (int f=1;f<=s;f++)
+		{
+			if(f%2!=0)
+			{
+				System.out.println("odd number is "+f);
+			}
+		}
+	}
+}
